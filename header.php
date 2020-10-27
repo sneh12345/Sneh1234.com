bcdb65d9986fe9581183d01ecafa8ffb47a01a88
sneh12345 <70284213+sneh12345@users.noreply.github.com>
2020-10-27 16:49:28 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,3 @@
 <head>
-<link rel='shortcut icon' type='image/x-icon' href='/poop.png.ico' />
+<link rel='shortcut icon' type='image/x-icon'https://thumbs.dreamstime.com/b/walking-pink-word-text-logo-icon-design-typography-suitable-card-147085425.jpg' />
 </head>
